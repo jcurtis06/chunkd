4c83a59c7a83483affe82d4b9569405437404006
BuildTools <unconfigured@null.spigotmc.org>
2022-01-05 14:30:51 -0500

    made a way to tell if a player has changed chunks

--- src/main/java/org/github/jcurtis/chunkd/Chunkd.java
+++ src/main/java/org/github/jcurtis/chunkd/Chunkd.java
@@ -3,6 +3,7 @@ package org.github.jcurtis.chunkd;
 import com.google.gson.Gson;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.github.jcurtis.chunkd.commands.ChunkCMD;
+import org.github.jcurtis.chunkd.events.ChangeChunkEvent;
 import org.github.jcurtis.chunkd.managers.ChunkManager;
 
 public final class Chunkd extends JavaPlugin {
@@ -15,6 +16,8 @@ public final class Chunkd extends JavaPlugin {
         this.chunkManager = new ChunkManager(this);
 
         this.getCommand("chunk").setExecutor(new ChunkCMD(this));
+
+        this.getServer().getPluginManager().registerEvents(new ChangeChunkEvent(this.chunkManager), this);
     }
 
     @Override
---
+++ src/main/java/org/github/jcurtis/chunkd/events/ChangeChunkEvent.java
@@ -0,0 +1,27 @@
+package org.github.jcurtis.chunkd.events;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.github.jcurtis.chunkd.managers.ChunkManager;
+
+public class ChangeChunkEvent implements Listener {
+    private final ChunkManager chunkManager;
+
+    public ChangeChunkEvent(ChunkManager chunkManager) {
+        this.chunkManager = chunkManager;
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+
+        if (event.getFrom().getChunk() != event.getTo().getChunk()) {
+            if (chunkManager.getOwner(event.getTo().getChunk()) != null) {
+                player.sendMessage(ChatColor.GREEN + "You have entered " + chunkManager.getOwner(event.getTo().getChunk()).getName() + "'s territory.");
+            }
+        }
+    }
+}
